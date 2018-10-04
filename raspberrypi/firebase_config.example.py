d63b8e431408d806b2a8496f042b6308d4af1265
Erick <eri.lewo@hotmail.com>
2018-10-03 22:39:37 -0400

    changed to use rx concurrency and automatic thread scheduler

--- raspberrypi/firebase_config.example.py
+++
@@ -1,6 +0,0 @@
-config = {
-    "apiKey": "apiKey",
-    "authDomain": "projectId.firebaseapp.com",
-    "databaseURL": "https://databaseName.firebaseio.com",
-    "storageBucket": "projectId.appspot.com"
-}
--- raspberrypi/run.py
+++ raspberrypi/run.py
@@ -1,20 +1,24 @@
-from .source.EnvironmentDataSource import EnvironmentDataSource
-from .controller import PlantController
-from threading import Thread, Lock
-import time
+import multiprocessing
+from threading import Lock
+from rx import Observable
+from rx.concurrency import ThreadPoolScheduler
+
+from source.EnvironmentDataSource import EnvironmentDataSource
+from source.controller import PlantController
+from DataObserver.SensorObserver import SensorObserver
+
+optimal_thread_count = multiprocessing.cpu_count()
+scheduler = ThreadPoolScheduler(optimal_thread_count)
 
 mutex = Lock()
-
+observer = SensorObserver()
 controlDevice = PlantController('/dev/ttyUSB0', 9600, mutex)
-data = EnvironmentDataSource(controlDevice.dev, mutex, observer)
+data = EnvironmentDataSource(controlDevice.dev, mutex)
 
-t = Thread(target=data.get_sensor_values)
-t.start()
+Observable.create(data.get_sensor_values) \
+        .subscribe_on(scheduler) \
+        .subscribe(observer)
 
-time.sleep(20)
-t2 = Thread(target=controlDevice.waterPlant())
-t2.start()
-
-time.sleep(20)
-t2 = Thread(target=controlDevice.waterPlant(1))
-t2.start()
+Observable.just("waterPlant") \
+        .subscribe_on(scheduler) \
+        .subscribe(on_next=lambda i: controlDevice.waterPlant())
